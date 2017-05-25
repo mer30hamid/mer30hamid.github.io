e110bdd739d2613ece3394d8a4db34eb4af051f0
mer30hamid <mer30hamid@gmail.com>
2017-05-25 09:52:22 +0430

    Update main.js

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ $(document).ready(function () {
     currentWidth = $('.panel-cover').width()
     if (currentWidth < 960) {
       $('.panel-cover').addClass('panel-cover--collapsed')
-      $('.content-wrapper').addClass('animated slideInRight')
+      $('.content-wrapper').addClass('animated slideInLet')
     } else {
       $('.panel-cover').css('max-width', currentWidth)
       $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
