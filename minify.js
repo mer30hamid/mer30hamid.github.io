825ac232e5deb009de114904ad0ea41582b1cf8e
mer30hamid <mer30hamid@gmail.com>
2017-06-29 10:56:23 +0430

    correct css seq combile js files

--- minify.js
+++ minify.js
@@ -1,48 +1,39 @@
+/*
+https://github.com/srod/node-minify
+*/
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 var compressor = require('node-minify');
 
-compressor.minify({
-  compressor: 'csso',
-  input: ['./css/main.css', './css/uno.css', './css/animate.css','./css/monokai.css'],
-  output: './css/main.min.css',
-  options: {
-    restructureOff: true // turns structure minimization off 
-  },
-  callback: function (err, min) {}
+rl.question('minify css files? ', (answer) => {
+  // TODO: Log the answer in a database
+  if ((answer.toLowerCase() == 'y') || (answer.toLowerCase() == 'yes')){
+	  console.log(`your answer: ${answer} . processing CSS files ...`);
+    compressor.minify({
+      compressor: 'csso',
+      input: ['./css/animate.css','./css/uno.css','./css/monokai.css','./css/main.css'],
+      output: './css/main.min.css',
+      options: {
+        restructureOff: true // turns structure minimization off 
+      },
+      callback: function (err, min) {}
+    });
+ }
+ rl.question('minify js files? ', (answer) => {
+  // TODO: Log the answer in a database
+  if ((answer.toLowerCase() == 'y') || (answer.toLowerCase() == 'yes')){
+	  console.log(`your answer: ${answer} . processing JS files...`);
+    compressor.minify({
+      compressor: 'uglifyjs',
+      input: ['./js/jquery.min.js','./js/jquery.timeago.min.js','./js/main.js'],
+      output: './js/main.min.js',
+      callback: function (err, min) {}
+    });
+  }
+  rl.close();
+  });
+ //rl.close();
 });
-
-
-compressor.minify({
-  compressor: 'uglifyjs',
-  input: './js/main.js',
-  output: './js/main.min.js',
-  callback: function (err, min) {}
-});
-
-
-/*
-compressor.minify({
-  compressor: 'yui-js',
-  input: './js/main.js',
-  output: './js/main.min.js',
-  options: {
-    'line-break': 80,
-    charset: 'utf8'
-  },
-  callback: function (err, min) {}
-});
-*/
-
-/*
-compressor.minify({
-  compressor: 'gcc',
-  input: './js/main.js',
-  output: './js/main.min.js',
-  options: {
-    compilation_level: 'ADVANCED_OPTIMIZATIONS',
-    language: 'ECMASCRIPT6',
-	closure_output_charset: 'utf8'
-    
-  },
-  callback: function (err, min) {}
-});
-*/
