bfff33f8a3c6ca8c09c7ba98c66dd798689dcf35
mer30hamid <mer30hamid@gmail.com>
2017-06-30 14:02:45 +0430

    cdn ready

--- minify.js
+++ minify.js
@@ -2,38 +2,46 @@
 https://github.com/srod/node-minify
 */
 const readline = require('readline');
+var fs = require('fs');
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 var compressor = require('node-minify');
+var jsOutputFile = './js/main.min.js';
+var cssOutputFile = './css/main.min.css';
+var cdnLocalPath = '../cdn.takwp.ir/docs/';
 
 rl.question('minify css files? ', (answer) => {
   // TODO: Log the answer in a database
   if ((answer.toLowerCase() == 'y') || (answer.toLowerCase() == 'yes')){
-	  console.log(`your answer: ${answer} . processing CSS files ...`);
+	console.log(`your answer: ${answer} . processing CSS files ...`);
     compressor.minify({
       compressor: 'csso',
       input: ['./css/animate.css','./css/uno.css','./css/monokai.css','./css/main.css'],
-      output: './css/main.min.css',
+      output: cssOutputFile,
       options: {
         restructureOff: true // turns structure minimization off 
       },
       callback: function (err, min) {}
     });
+	if (cdnLocalPath)
+		fs.createReadStream(cssOutputFile).pipe(fs.createWriteStream(cdnLocalPath+'css/main.min.css'));
  }
  rl.question('minify js files? ', (answer) => {
   // TODO: Log the answer in a database
   if ((answer.toLowerCase() == 'y') || (answer.toLowerCase() == 'yes')){
-	  console.log(`your answer: ${answer} . processing JS files...`);
+	console.log(`your answer: ${answer} . processing JS files...`);
     compressor.minify({
       compressor: 'uglifyjs',
       input: ['./js/jquery.min.js','./js/jquery.timeago.min.js','./js/main.js'],
       output: './js/main.min.js',
       callback: function (err, min) {}
     });
+	if (cdnLocalPath)
+		fs.createReadStream(jsOutputFile).pipe(fs.createWriteStream(cdnLocalPath+'js/main.min.js'));
+
   }
   rl.close();
   });
- //rl.close();
 });
